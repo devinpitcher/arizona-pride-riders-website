83718c2bbf3de182f3d83912fe2f55df76daee0c
Devin Pitcher <devin@devinpitcher.com>
2024-08-24 17:03:40 -0700

    Extract form

---
+++ app/components/EmailSignupForm.tsx
@@ -0,0 +1,24 @@
+export const EmailSignupForm = () => {
+  return (
+    <form className="flex-col mx-auto mt-10 flex max-w-md gap-4 w-full sm:flex-row">
+      <label htmlFor="email-address" className="sr-only">
+        Email address
+      </label>
+      <input
+        id="email-address"
+        name="email"
+        type="email"
+        autoComplete="email"
+        required
+        className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
+        placeholder="Enter your email"
+      />
+      <button
+        type="submit"
+        className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+      >
+        Notify me
+      </button>
+    </form>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logoImage from "../public/logo.png";
+import { EmailSignupForm } from "@/app/components/EmailSignupForm";
 
 export default function Home() {
   return (
@@ -36,26 +37,7 @@ export default function Home() {
         </p>
       </div>
 
-      <form className="mx-auto mt-10 flex max-w-md gap-x-4 w-full">
-        <label htmlFor="email-address" className="sr-only">
-          Email address
-        </label>
-        <input
-          id="email-address"
-          name="email"
-          type="email"
-          autoComplete="email"
-          required
-          className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-          placeholder="Enter your email"
-        />
-        <button
-          type="submit"
-          className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-        >
-          Notify me
-        </button>
-      </form>
+      <EmailSignupForm />
     </div>
   );
 }
