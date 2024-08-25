a1fd38db2cebb24c5f6eacf693cc2c77589133a7
Devin Pitcher <devin@devinpitcher.com>
2024-08-24 17:00:36 -0700

    Set up page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import forms from "@tailwindcss/forms";
 
 const config: Config = {
   content: [
@@ -12,9 +13,14 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        rainbow:
+          "linear-gradient(109deg, rgba(54,123,239,1) 0%, rgba(255,111,99,1) 40%, rgba(196,233,0,1) 80%, rgba(0,197,114,1) 100%)",
+      },
+      fontFamily: {
+        bangers: ["var(--font-bangers)"],
       },
     },
   },
-  plugins: [],
+  plugins: [forms],
 };
 export default config;
